f861916eef2293e10d6fa9662d06af29fd3e6c05
Akshay-368 <gargakshay2004@gmail.com>
2026-05-04 11:40:59 +0530

    Removed core-pythonfiles that were accidentally added to collections branch

--- core-python-practice/PythonDataTypesAssignement/Level-1/Problem3.py
+++
@@ -1,7 +0,0 @@
-# problem 3 levle 1
-# making a tuple with top 3 programming languages
-# then printing them in one nice sentence
-
-langs = ( "Python", "Java", "C++")   # tuple because we dont want to change it
-
-print ( "My top 3 programming languages are" , langs[0] , "," , langs[1] , "and" , langs[2] )
